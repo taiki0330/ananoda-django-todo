2cd32ae94c3cfb1f21f56a0af3c5d0107604f3ed
taiki0330 <jonas.taiki.0330@icloud.com>
2024-02-23 16:52:22 +0900

    first commit

---
+++ todo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import TodoModel
+
+# Create your views here.
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = TodoModel
+
+class TodoDetail(DetailView):
+    model = TodoModel
+    template_name = "detail.html"
+
+
+
+
+
